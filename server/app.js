92214f2cd0c97e81d8443791d39d10ba4007ae87
Edgar Harwood <edghar88@gmail.com>
2021-07-25 17:57:15 -0600

    dist

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const distDir = __dirname + "/dist/";
 const mongo = require('./util/mongo');
 const PORT = process.env.PORT || 3001;
 
@@ -7,6 +8,7 @@ mongo.connectToServer((err, _) => {
   if (err) {
     console.log(err);
   }
+  app.use(express.static(distDir));
   app.use(require('body-parser').json());
   app.use(require('./routes'));
   app.listen(PORT, () => {
